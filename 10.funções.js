402d1f0c9f45207daea6ae56858cc8a396d31182
Isadora Neves dos Santos <doraneves06@gmail.com>
2026-02-26 21:39:51 -0300

    função

---
+++ 10.funções.js
@@ -0,0 +1,28 @@
+// FUNÇÃO
+
+function somar  ( a, b) {
+    return a + b
+}
+
+const soma = somar(2, 3)
+console.log(`A soma é: ${soma}`)
+
+const subtracao = function (a, b) {
+    return a - b
+}
+
+const subtração = subtracao(2, 3)
+console.log(`A subtração é: ${subtração}`)
+
+const multiplicação = function (a, b) {
+    return a * b
+}
+
+const multiplicaçãoResultado = multiplicação(2, 3)
+console.log(`A multiplicação é: ${multiplicaçãoResultado}`)
+
+const divisão = function (a, b) {
+console.log(`A divisão é: ${divisão(a, b)}`)    
+return a / b
+
+}
